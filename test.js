8300fcfa4b265f0371cc885e5f4d54f33f5bc77b
Betty Seifu <betty.seifu149@gmail.com>
2017-08-15 14:50:00 +0300

    testing

---
+++ test.js
@@ -0,0 +1,5 @@
+var target = document.querySelector('new');
+var div = document.createElement('div');
+div.innerHTML = 'a new element is created';
+target.parentNode.insertBefore( div, target );
+target.parentNode.insertBefore( div, target.nextSibling );
